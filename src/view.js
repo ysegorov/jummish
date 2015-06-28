13de28581e09c60ff74dff54ca6f7c76224d1ea5
Yuri Egorov <ysegorov@gmail.com>
2015-06-28 10:44:56 +0300

    a bit of View docs

--- src/view.js
+++ src/view.js
@@ -13,8 +13,20 @@ function mkToken() {
 }
 
 
+/**
+ * Extendable View boilerplate
+ *
+ **/
 var View = Proto.extend({
 
+    /**
+     * View constructor function
+     * will be invoked by Proto.new method
+     *
+     * @param {object} props - View properties
+     * @param {Element} [targetEl] - DOM Element to mount View instance to
+     *
+     */
     constructor: function constructor(props, targetEl) {
         this.props = props || {};
         this.cid = mkToken();
@@ -32,8 +44,9 @@ var View = Proto.extend({
     },
     // dom helpers
     $: $,
-    // template name
+    /** template name for a View instance */
     template: '*',
+    /** View close method */
     close: function close() {
         this.unmount();
         this.removeHandlers();
@@ -44,18 +57,44 @@ var View = Proto.extend({
 
         return this;
     },
-    // extension point
+    /**
+     * Method to serialize data to render a View template with
+     *
+     * Extension point, must be overriden
+     *
+     * @return {object} - object to be passed to template function
+     *
+     */
     serialize: function serialize() {
         return {};
     },
+    /**
+     * Method to load template from a pool of available templates
+     *
+     * Must be overriden to customize template loading
+     *
+     * @param {string} tmplName - Name of the template to load
+     * @return {function} - function to be called to render template
+     *
+     */
     loadTemplate: function() {
         throw new Error('Subclass must provide ``loadTemplate`` method');
     },
-    // extension point
+    /**
+     * Method to render template using serialized data
+     *
+     * Can be overriden to customize renderer
+     *
+     * @return {string} - rendered html string
+     *
+     */
     render: function render() {
         var tmpl = this.loadTemplate(this.template);
         return tmpl(this.serialize());
     },
+    /**
+     * Method to re-render view if needed
+     */
     reRender: function reRender() {
         var targetEl = this.el.parentNode,
             props = this.props;
@@ -65,6 +104,12 @@ var View = Proto.extend({
 
         return this.mountTo(targetEl);
     },
+    /**
+     * Method to handle DOM event and call actual event handler
+     *
+     * @param {Event} evt - DOM event
+     *
+     */
     handleEvent: function handleEvent(evt) {
         var handlers = this.events[evt.type],
             target = evt.target,
@@ -90,6 +135,14 @@ var View = Proto.extend({
             }
         });
     },
+    /**
+     * Method to mount view to DOM element
+     *
+     * Automatically renders view if needed
+     *
+     * @param {Element} targetEl - DOM element to mount view to
+     *
+     */
     mountTo: function mountTo(targetEl) {
         if (!this.el) {
             this._beforeMount();
@@ -107,15 +160,32 @@ var View = Proto.extend({
 
         return this._afterMount();
     },
-    _populateNodes: function _populateNodes() {
-        if (!this.el) {
-            throw new Error('View.populateNodes must be called after View.el was created');
-        }
-        if (_.isFunction(this.populateNodes)) {
-            this.populateNodes();
-        }
+    /**
+     * Method to unmount (detach ``this.el`` from DOM) view
+     *
+     * @return {object} this
+     *
+     */
+    unmount: function unmount() {
+        this._beforeUnmount();
+
+        $.remove(this.el);
+
+        this._afterUnmount();
+
         return this;
     },
+    /**
+     * Method to add event listeners to ``this.el``
+     *
+     * For ``focus`` and ``blur`` events attaches handlers to currently
+     * available in DOM nodes (as these events don't bubble)
+     *
+     * Requires View instance to have method ``this.handleEvent``
+     *
+     * @return {object} this
+     *
+     */
     populateHandlers: function populateHandlers() {
         var el = this.el,
             events = this.events;
@@ -140,7 +210,17 @@ var View = Proto.extend({
             });
         })(el, this, events);
 
+        return this;
     },
+    /**
+     * Method to remove event listeners attached to ``this.el``
+     *
+     * For ``focus`` and ``blur`` events detaches handlers from currently
+     * available in DOM nodes (as these events don't bubble)
+     *
+     * @return {object} this
+     *
+     */
     removeHandlers: function removeHandlers() {
         var el = this.el,
             events = this.events;
@@ -164,7 +244,28 @@ var View = Proto.extend({
             });
         })(el, this, events);
 
+        return this;
     },
+    /**
+     * Method to mount children views, described in ``this.children`` object,
+     * to nodes, described in ``this.nodes`` object
+     *
+     * Expected structure:
+     *
+     * this.chidlren = {
+     *      $menu: MenuView.new({}),
+     *      $items: [ItemView.new({item: 1}), ItemView.new({item: 2})]
+     * };
+     * this.nodes = {
+     *      $menu: $('[data-menu]', this.el),
+     *      $items: $('[data-items]', this.el)
+     * }
+     *
+     * Method is called automatically in ``this.mountTo`` method.
+     *
+     * @return {object} this
+     *
+     */
     mountChildren: function mountChildren() {
         var ch = this.children,
             nodes = this.nodes;
@@ -187,6 +288,12 @@ var View = Proto.extend({
         }
         return this;
     },
+    /**
+     * Method to close children views, described in ``this.children`` method
+     *
+     * @return {object} this
+     *
+     */
     closeChildren: function closeChildren() {
         var ch = this.children;
 
@@ -209,37 +316,42 @@ var View = Proto.extend({
         }
         return this;
     },
+    /* Internal method to call ``this.populateNodes`` if available */
+    _populateNodes: function _populateNodes() {
+        if (!this.el) {
+            throw new Error('View.populateNodes must be called after View.el was created');
+        }
+        if (_.isFunction(this.populateNodes)) {
+            this.populateNodes();
+        }
+        return this;
+    },
+    /* Internal method to call ``this.beforeMount`` if available */
     _beforeMount: function _beforeMount() {
         if (_.isFunction(this.beforeMount)) {
             this.beforeMount();
         }
         return this;
     },
+    /* Internal method to call ``this.afterMount`` if available */
     _afterMount: function _afterMount() {
         if (_.isFunction(this.afterMount)) {
             this.afterMount();
         }
         return this;
     },
+    /* Internal method to call ``this.beforeUnmount`` if available */
     _beforeUnmount: function _beforeUnmount() {
         if (_.isFunction(this.beforeUnmount)) {
             this.beforeUnmount();
         }
         return this;
     },
+    /* Internal method to call ``this.afterUnmount`` if available */
     _afterUnmount: function _afterUnmount() {
         if (_.isFunction(this.afterUnmount)) {
             this.afterUnmount();
         }
-        return this;
-    },
-    unmount: function unmount() {
-        this._beforeUnmount();
-
-        $.remove(this.el);
-
-        this._afterUnmount();
-
         return this;
     }
 
